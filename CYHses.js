b929fa7f5e4b18ce16985b6ab340d6434caa7312
CYHses <157192975+CYHses@users.noreply.github.com>
2024-01-26 10:12:51 +0630

    Update CYHses.js

--- CYHses.js
+++ CYHses.js
@@ -1,17 +1,18 @@
 function check(){
+	const button=document.querySelector("#loginButton");
 	const el=document.getElementById("check");
 	const check1=document.getElementById("check1");
 	const check2=document.getElementById("check2");
 	const check3=document.getElementById("check3");
-	const firstname=document.getElementById("input1").value;
-	const lastname=document.getElementById("input2").value;
+	const filename=document.getElementById("input1").value;
+	const basename=document.getElementById("input2").value;
 	const password=document.getElementById("input3").value;
 	
 	
-	if (firstname==="") {
+	if (filename==="") {
 		check1.innerHTML="*";
 		el.innerHTML="please check first name *";
-	}else if(lastname===""){
+	}else if(basename===""){
 		check1.innerHTML="";
 		check2.innerHTML="*";
 		el.innerHTML="please check last name *";
@@ -25,7 +26,12 @@ function check(){
 	check2.innerHTML="";
 	check3.innerHTML="";
 	el.innerHTML="";
+	button.innerHTML="<a href='CYHsesHome.html'>Login</a>";
 	
 	}
 };
-	
+
+function brownMode(){
+	const brownMode=document.querySelector("body");
+	brownMode.classList.toggle("brownMode");
+}
